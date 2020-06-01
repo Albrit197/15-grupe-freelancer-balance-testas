2c0a4b1de8b4031dd68ac42822c627a8bc7df6c7
Albrit197 <63236021+Albrit197@users.noreply.github.com>
2020-06-01 14:12:21 +0300

    Calculations unfinished

---
+++ js/home.js
@@ -0,0 +1,31 @@
+const Sausis = 200;
+const Vasaris = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+const Sausis = 200;
+
+
+
+const menesioIplaukos = [
+    Sausis,
+    Vasaris,
+    Kovas,
+    Balandis,
+    Geguzis,
+    Birzelis,
+    Liepa,
+    Rugpjutis,
+    Rugsejis,
+    Spalis,
+    Lapkritis,
+    Gruodis
+]
+
+console.log(menesioIplaukos);
